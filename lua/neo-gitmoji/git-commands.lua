0372fcac758735f82752f7f048adb78d1f042085
Luca Zulberti <zulberti.luca@gmail.com>
2025-01-16 11:03:12 +0100

    :sparkles: Use fugitive if available

--- lua/neo-gitmoji/git-commands.lua
+++ lua/neo-gitmoji/git-commands.lua
@@ -1,5 +1,11 @@
 local Git = {}
 
-function Git.commit(title) vim.cmd(string.format('!Git commit -m "%s"', title)) end
+function Git.commit(title)
+    if not pcall(vim.cmd, "Git") then
+        vim.cmd(string.format('!git commit -m "%s"', title))
+    else
+        vim.cmd(string.format(':Git commit -m "%s"', title))
+    end
+end
 
 return Git
